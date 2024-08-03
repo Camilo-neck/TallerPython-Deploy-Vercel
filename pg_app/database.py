b5f02798dab11022e81fd6ee8d1200f9ab1608b7
Camilo-neck <ccuello@unal.edu.co>
2024-08-03 01:51:06 -0500

    set database url on .env

--- pg_app/database.py
+++ pg_app/database.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin1234@localhost:5432/designpatterns"
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, 
